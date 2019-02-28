0b83a17bfdcca6b73571e23199980448bdb5769c
Daniel Ale <danielale9291@gmail.com>
2019-02-28 12:46:55 +0100

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+import Pages from "./pages";
+
+const apiUrl = "https://server-qwioa2xeo.now.sh";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+	uri: "http://localhost:4000/"
+});
+const client = new ApolloClient({
+	cache,
+	link
+});
+
+client
+	.query({
+		query: gql`
+			query GetLaunch {
+				launch(id: 56) {
+					id
+					mission {
+						name
+					}
+				}
+			}
+		`
+	})
+	.then((result) => console.log(result));
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<ApolloProvider client={client}>
+				<Pages />
+			</ApolloProvider>
+		);
+	}
 }
 
 export default App;
