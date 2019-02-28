ee8f30139faa246d83d1fbb3738e8376bcde275a
Daniel Ale <danielale9291@gmail.com>
2019-02-28 13:09:09 +0100

    Fix -- Corrected all import issues

--- src/components/launch-detail.js
+++ src/components/launch-detail.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import styled from 'react-emotion';
+import React from "react";
+import styled from "@emotion/styled";
 
-import { unit } from '../styles';
-import { cardClassName, getBackgroundImage } from './launch-tile';
+import { unit } from "../styles";
+import { cardClassName, getBackgroundImage } from "./launch-tile";
 
 const LaunchDetail = ({ id, site, rocket }) => (
-  <Card
-    style={{
-      backgroundImage: getBackgroundImage(id),
-    }}
-  >
-    <h3>
-      {rocket.name} ({rocket.type})
-    </h3>
-    <h5>{site}</h5>
-  </Card>
+	<Card
+		style={{
+			backgroundImage: getBackgroundImage(id)
+		}}
+	>
+		<h3>
+			{rocket.name} ({rocket.type})
+		</h3>
+		<h5>{site}</h5>
+	</Card>
 );
 
 /**
  * STYLED COMPONENTS USED IN THIS FILE ARE BELOW HERE
  */
 
-const Card = styled('div')(cardClassName, {
-  height: 365,
-  marginBottom: unit * 4,
+const Card = styled("div")(cardClassName, {
+	height: 365,
+	marginBottom: unit * 4
 });
 
 export default LaunchDetail;
